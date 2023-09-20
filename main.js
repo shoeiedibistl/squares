e3a2b3c7c24ba5edaccab4373da64f3ed52ba19c
shoeiedibistl <127901044+shoeiedibistl@users.noreply.github.com>
2023-09-21 02:38:47 +0300

    added opacity

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ function getColor () {
     let r = getRandomNumber(0, 255);
     let g = getRandomNumber(0, 255);
     let b = getRandomNumber(0, 255);
+    
+    let opacity = getRandomNumber(1, 10) / 10;
 
-    return `rgb(${r}, ${g}, ${b})`
+    return [`rgb(${r}, ${g}, ${b})`, opacity]
 }
 
 function getCoordinates(myWidth, myHeight) {
@@ -18,18 +20,24 @@ function getCoordinates(myWidth, myHeight) {
     return [x, y]
 }
 
+function getOpacity () {
+}
+
 const num = getRandomNumber(10, 100);
 
 function addSquares (num) {
     const parent = document.querySelector(".div")
     let coordinates
+    let color
 
     for (let i = 0; i < num; i++) {
         let newSquare = document.createElement("div");
+        color = getColor ()
         coordinates = getCoordinates(window.innerWidth - 40, window.innerHeight - 40);
         parent.append(newSquare)
-        newSquare.classList.add ("mySquare")
-        newSquare.style.backgroundColor = getColor();
+        newSquare.classList.add ("mySquare");
+        newSquare.style.backgroundColor = color[0];
+        newSquare.style.opacity = color[1];
         newSquare.style.transform = `translate(${coordinates[0]}px, ${coordinates[1]}px)`
     }
 }
